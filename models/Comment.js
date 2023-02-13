af98233370b570c01c4ed10f0df3ba2427f7cf8d
krausyd (∩｀-´)⊃━☆ﾟ.*･｡ﾟ <krausyd@hotmail.com>
2023-02-12 19:42:30 -0800

    Add format date when getting pizzas and comments

--- models/Comment.js
+++ models/Comment.js
@@ -1,17 +1,26 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const CommentSchema = new Schema({
-    writtenBy: {
-        type: String,
+const CommentSchema = new Schema(
+    {
+        writtenBy: {
+            type: String,
+        },
+        commentBody: {
+            type: String,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal),
+        },
     },
-    commentBody: {
-        type: String,
+    {
+        toJSON: {
+            getters: true,
+        },
     },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-});
+);
 
 const Comment = model('Comment', CommentSchema);
 
